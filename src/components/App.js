dc7f23fbc09e5058170a1ffe8ed416f4d4e85566
Imran shaikh <imranpathan7040@gmail.com>
2024-02-19 13:11:24 +0530

    code update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import Form from "./Form";
 
 const App = () => {
+
+  const [isLoggedIn,setIsLoggedIn]  = useState(false);
   return (
     <div>
-        {/* Do not remove the main div */}
+        {/* Do not remove the main div */}{
+
+          !isLoggedIn &&  <Form  isLoggedIn={isLoggedIn} setIsLoggedIn ={setIsLoggedIn} />
+     
+        }
+          <p> {isLoggedIn ? 'You are logged In' : ''}</p>
     </div>
   )
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+function Form({isLoggedIn,setIsLoggedIn}) {
+    const [userName,setUserName] = useState("");
+    const [password,setPassword] = useState("");
+
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        setIsLoggedIn(!isLoggedIn);
+
+        alert("logged In")
+
+    }
+  return (
+    <div>
+        <form action="">
+            <label htmlFor="">UserName:</label>
+            <input type="text"  value={userName} onChange={e=>setUserName(e.target.value)} />
+            <br />
+
+            <label htmlFor="">Password:</label>
+            <input type="password" value={password} onChange={e=>setPassword(e.target.value)} /><br />
+            <button onClick={handleSubmit} className="login">Login</button>
+        </form>
+    </div>
+  )
+}
+
+export default Form
